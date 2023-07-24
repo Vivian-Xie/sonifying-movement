93a064ad6ea1ce78882477fd92da046a9c6db874
Vivian Xie <xx2185@nyu.edu>
2023-07-24 12:45:09 +0800

    for updating new server

--- server.py
+++ server.py
@@ -1,29 +1,56 @@
 from socket import *
+import threading
+import time
 
 serverSocket=socket(AF_INET,SOCK_STREAM)
-#serverSocket.bind(("10.209.92.109",1200))
-serverSocket.bind(("192.168.99.132",1200))
-#serverSocket.bind(("127.0.0.1",1200))
-#serverSocket.bind(("10.27.18.22",1200))
+serverSocket.bind(("10.209.92.109",1200))
+#serverSocket.bind(("192.168.1.12",1200))
 
-serverSocket.listen(125)
+serverSocket.listen(20)
 print("the server is ready to accept info...")
 connectionSocket,address=serverSocket.accept()
 print("client 1 online")
 connectionSocket2,address2=serverSocket.accept()
 print("client 2 online")
+message=''
 
-emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
-while True:
-    message=connectionSocket.recv(1024).decode()
-    
-    print("got the message from the client: "+message)
-    if message and connectionSocket2:
-        if not (message in emotions):
-            message=message
-        connectionSocket2.send((message).encode())
-        #sconnectionSocket.send((message+"haha").encode())
-    elif not connectionSocket2:
-        break
-connectionSocket.close()
+def message2music():
+    while True:
+        global message
+        if connectionSocket2:
+            if  message!="":
+                connectionSocket2.send((message).encode())
+                print("message to the music generation client",message)
+                message=""
+                time.sleep(3.75)
+                #sconnectionSocket.send((message+"haha").encode())
+            else:
+                connectionSocket2.send(("neutral").encode())
+                time.sleep(3.75)
+        else:
+            connectionSocket.close()
+            connectionSocket2.close()
+            break
+
+
+
+def listening2emo():
+    while True: 
+        global message
+        message=connectionSocket.recv(1024).decode()
+        print("got the message from the client: "+message)
+
+
+        
+
+def main():
+    thread1=threading.Thread(target=listening2emo,name="T1")
+    thread2=threading.Thread(target=message2music,name="T2")
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    print("all done")
+
+if __name__=='__main__':
+    main()
